e6e33a64826a8420dd764e550a034cfcac6d2bcb
Kaminskiy Dmitriy <demonidc@gmail.com>
2020-10-21 16:21:13 +0600

    update 21.10.2020

--- Homeworks/Control Constructs and Collections Part 1/Task 4.py
+++ Homeworks/Control Constructs and Collections Part 1/Task 4.py
@@ -8,24 +8,7 @@ countries_temperature = [
 ['Russia'], [35.6, 37.4, 39.2, 41, 42.8, 39.2, 35.6],
 ['Poland'], [50, 50, 53.6, 57.2, 55.4, 55.4]]
 
+for y in range (1, int(len(countries_temperature)), 2):
+    print(*countries_temperature[y-1], '-', (round(((((sum(countries_temperature[y])/len(countries_temperature[y]))-32)*5/9)), 1)), 'C')
 
-thailand = 0
-germany = 0
-russia = 0
-poland = 0
 
-for x in range(len(countries_temperature[1])):
-    thailand += countries_temperature[1][x]
-print(*countries_temperature[0], '-', round((5/9*((thailand/(len(countries_temperature[1])))-32)), 1), 'C')
-
-for x in range(len(countries_temperature[3])):
-    germany += countries_temperature[3][x]
-print(*countries_temperature[2], '-', round((5/9*((germany/(len(countries_temperature[3])))-32)), 1), 'C')
-
-for x in range(len(countries_temperature[5])):
-    russia += countries_temperature[5][x]
-print(*countries_temperature[4], '-',round((5/9*((russia/(len(countries_temperature[5])))-32)), 1), 'C')
-
-for x in range(len(countries_temperature[7])):
-    poland += countries_temperature[7][x]
-print(*countries_temperature[6], '-', round((5/9*((poland/(len(countries_temperature[7])))-32)), 1), 'C')
